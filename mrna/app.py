b58ecfd67dde73ed406bd9b9841581b2e90c4fc7
Faris Chebib <octaflop@gmail.com>
2012-03-09 00:28:18 -0800

    added reqs and template and exp app

--- mrna/app.py
+++ mrna/app.py
@@ -1,15 +1,16 @@
 from flask import Flask
+app = Flask(__name__)
 
-from mrna.views import contactform
+from views import mail_admin
 from forms import ContactForm
-
-app = Flask.app(__name__)
+from decorators import template
 
 #from mrna.settings import ADMIN
-
+@template('templates/base.html')
 @app.route('/contact/')
 def contact_form(request, *args, **kwargs):
     form = ContactForm(request.form)
+    msg = {}
     if form.validate_on_submit():
         subject = form.subject.data
         message = form.message.data
@@ -19,6 +20,8 @@ def contact_form(request, *args, **kwargs):
         if ret[succ_code] != 0:
             flash("There was an error sending this message")
             return redirect(app.route('/'))
+    ret = dict(request=request, args=args, kwargs=kwargs, msg=msg)
     return ret
 
-
+if __name__ == "__main__":
+    app.run()
--- mrna/forms.py
+++ mrna/forms.py
@@ -1,13 +1,16 @@
 from flaskext.wtf import Form, BooleanField, TextField, validators,\
 PasswordField, FileField, TextAreaField, DateTimeField, RecaptchaField,\
-HiddenField, DateField, EmailField
+HiddenField, DateField
 
 from flaskext.babel import gettext as _
 from flaskext.babel import ngettext as _n
 
 class ContactForm(Form):
     subject = TextField(_("Subject"),\
-                [validators.required(),\
+                [validators.Required(),\
                 validators.length(min=3, max=200)])
-    sender = EmailField(_("Your Email"))
+    sender = TextField(_("Your Contact Email Address"),\
+            [validators.required(),\
+            validators.length(min=3, max=100),\
+            validators.Email()])
     message = TextAreaField(_("Message"))
